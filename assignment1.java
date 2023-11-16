cc5a228dd69035bacc65a4e94ba43d1501d7cbb3
sohan7874 <144907081+sohan7874@users.noreply.github.com>
2023-11-16 23:43:52 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,123 @@
+package com.mod1;
+
+public class assignment1 {
+	 public static void main(String args[]) {
+//task 1- my information
+	/*
+	
+		String s="sohan";
+		int age=21;
+		String address="rajkot kothariya behind ranuja temple";
+		System.out.println(s);
+		System.out.println("14-11-2002");
+		System.out.println(age);
+		System.out.println(address);
+		*/
+		
+		 
+		 //task 2 - write a programee to add ,sub,mul,division, with two num
+		 
+		/* int a =10;
+		 int b=5;
+		 int sum=a+b;
+		 int sub=a-b;
+		 int mul=a*b;
+		 int div=a/b;
+		 System.out.println(sum);
+		 System.out.println(sub);
+		 System.out.println(mul);
+		 System.out.println(div);
+		 */
+		 
+		 
+		 //task 3 - write a programe to give a num a squre and cube
+		 
+		 /*int n = 10;
+		 int a=n*n;
+		 int b=n*n*n;
+		 System.out.println(a);
+         System.out.println(b);
+         */
+		 
+		 
+		 //task 4-write a programe to finde a area of circle
+		 
+		/* int r =5;
+	double area =(2*3.14*r);
+		System.out.println(area);
+		*/
+		 
+		 //task -5 --find area  of triangle
+		 
+		/* int hight=10;
+		 int bre=12;
+		 int area = (hight*bre)/2;
+		 System.out.println(area);
+		 */
+		 
+		 //task-6--write a programe simple intrest
+		 
+		 	/*	 int p=100;
+				 int r=10;
+				 int n=2;
+				 float intrest= (p*r*n)/100;
+				 System.out.println(intrest);
+		  */
+		 
+		 //task -7 -- write a progame a calculate 5 sub sum and pr
+		 
+		/* int maths=100;
+		 int phy=56;
+		 int chem=89;
+		 int bio=98;
+		 int com=33;
+		 int total=maths+phy+bio+chem+com;
+		 float percentage=total/5;
+		 System.out.println(total);
+		 System.out.println(percentage);
+		 
+		 */
+		 
+		 
+		 //task 8 -- check num is given poositive or nagativve
+	/*	 
+		 int  num= -5;
+		 if(num>0) {
+			 
+			 System.out.println(" num is possitive");
+		 }
+		 else {
+			 
+			 System.out.println("num is negative");
+		 }
+		*/
+		 
+		 
+		
+		// task-9 --given years leap year or not 
+		 
+		/* int year =1920;
+		 
+		 if(year%4==0){
+			 System.out.println("yes this is leap year");
+		 }
+		 else {
+			 System.out.println("no this is not a leap year");
+		 }
+		 
+	 
+	*/
+	
+		 //task 10 --write a program given num is prime or not
+		 
+		/* int num=15;
+		 if (num%2==0) {
+			 System.out.println("yes this is even num");
+		 }
+			 else{
+				System.out.println("num is odd");
+			 }
+			 */
+		 }
+		 }	 
+
